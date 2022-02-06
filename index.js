b59bb4b986d9dfc862b7d3a154c47eb96f9cc9ad
jowal-islam <ranabrindabon@gmail.com>
2022-02-06 23:54:29 +0600

    sum of digits

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 
-function calculate(r) {
-    var i = 3.141592653;
-    var squire = r * r;
-    var area = i * squire;
-    console.log(area);
+//sum of digit
+    let num = 2564;
+    let sum = 0;
+let temp = num;
+while (temp > 0) {
+    var r = temp % 10;
+ sum = sum + r;
+temp = parseInt(temp / 10);
 }
-calculate(2);
+console.log(sum);
